2adc690b4449ef8be0903dedaf5432af5f13c9cd
iotidev <iotidevelopment@gmail.com>
2019-08-10 02:34:21 -0300

    Implementação Real Time

--- src/server.js
+++ src/server.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server( app );
+const io = require('socket.io')( server );
 
-mongoose.connect('mongodb+srv://ridam:Ridam5533@cluster0-gtsrp.mongodb.net/tindev?retryWrites=true&w=majority', {
+const connectedUsers = {};
+
+io.on('connection', socket =>{
+  const { user } = socket.handshake.query;
+
+  connectedUsers[ user ] = socket.id;
+});
+
+mongoose.connect('mongodb+srv://ridam:Ridam5533@cluster0-gtsrp.mongodb.net/tindev5?retryWrites=true&w=majority', {
   useNewUrlParser: true,
 });
 
-server.use( cors() );
-server.use( express.json() );
-server.use( routes );
+app.use(( req, res, next ) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use( cors() );
+app.use( express.json() );
+app.use( routes );
 
 server.listen(3333);
